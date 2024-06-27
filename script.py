c96f09fd8ecca623267f3c7293b59bf309771c67
Schanen <r.schanen@lancaster.ac.uk>
2024-06-27 19:23:18 +0100

    add script

--- script.py
+++ script.py
@@ -42,8 +42,8 @@ TIME_INTERVAL = DATA[1, 0] - DATA[0, 0]
 # estimated signal frequency in Hertz
 APPROX_FREQ   = 96
 
-# compute an appoximate period length
-FRAME_LENGTH = int(1.25/APPROX_FREQ/TIME_INTERVAL)
+# compute an appoximate two and a half time the period length
+FRAME_LENGTH = int(2.50 / APPROX_FREQ / TIME_INTERVAL)
 
 print(f"""
 interval = {fEng(TIME_INTERVAL)}S 
@@ -52,7 +52,7 @@ frame 0 length = {fEng(FRAME_LENGTH)}
 """)
 
 #####################################################################
-#                                                      PLOT FRAME 0 #
+#                                                 PLOT FRAME 0 DATA #
 #####################################################################
 
 FRAME_NUMBER = 0
@@ -62,80 +62,64 @@ wp, ww = 0, FRAME_LENGTH
 # select data
 T, X = DATA[wp:wp+ww, 0] - DATA[wp, 0], DATA[wp:wp+ww, 1]
 
-
+# plot raw data
 fg, ax = stdFig(f"FRAME{FRAME_NUMBER}", T, "S", "Time", X, "V", "Signal")
 
-from scipy.signal import savgol_filter
-# trial and error filters:
-Y = savgol_filter(X, 50, 1)
-Y = savgol_filter(Y, 50, 1)
-Y = savgol_filter(Y, 50, 1)
-
-# find down zero crossings
-from numpy import where, diff, signbit
-I = Y > 0
-J = (I[:-1] & ~I[1:]).nonzero()[0]
-
-# exit if un-expected number zeros (there shoud be two)
-if not len(J) == 2:
-    print(f"number of down zero crossing {len(J)} ", end = "")
-    if len(J) > nc: print(" larger ", end = "")
-    if len(J) < nc: print(" smaller ", end = "")
-    print(f"than expected.")
-    print(f"exiting.")
-    exit()
-
-# get zeros indices
-i1, i2 = J
-
-print(f"approximate number of points per cycles = {i2-i1+1}")
-
-# compute pre-fitting paramneters
-# parameters are center position (positive zero crossing), period, amplitude
-t1, t2 = T[i1], T[i2]
-m1, m2 = min(Y), max(Y)
-P = [(t1+t2)/2.0, t2-t1, (m2-m1)/2]
-
 #####################################################################
-#                                                                   #
-#                         FITTING FUNCTION                          #
-#                                                                   #
+#                                              FIT FIRST FEW PERIOD #
 #####################################################################
 
-# in-phase fitting function
+# define fitting function
 def ff(t, p, w, h):
     from numpy import pi, sin
     x = 2*pi*(t-p)/w
     y = h*sin(x)
     return y
 
+# compute running average to find zeros
+from scipy.signal import savgol_filter
+# trial and error filters:
+Y = savgol_filter(X, 50, 1)
+Y = savgol_filter(Y, 50, 1)
+Y = savgol_filter(Y, 50, 1)
+ax.plot(T*fg.scx, Y*fg.scy, "-.k")
+
+# find down zero crossings
+from numpy import where, diff, signbit
+I = Y > 0
+J = (I[:-1] & ~I[1:]).nonzero()[0]
+ax.plot(T[J]*fg.scx, Y[J]*fg.scy,
+    'ko', markerfacecolor = 'white', markersize = 8)
+
+# get first two zeros indices only
+i1, i2 = J[0], J[1]
+print(f"approximate number of points per cycles = {i2-i1+1}")
+
+# compute pre-fitting paramneters:
+t1, t2 = T[i1], T[i2]       # time position [S]
+m1, m2 = min(Y), max(Y)     # signal extrema [V]
+
+# compute center position (positive zero crossing), period, amplitude
+P = [(t1+t2)/2.0, t2-t1, (m2-m1)/2]
+
 # fit first single cycle
 P, C = fit(ff, T, X, p0 = P)
+ax.plot(T*fg.scx, ff(T, *P)*fg.scy, "-k")
 
 # extract parameters
 p, w, h = P
 
-
-
-
-
-
-
-
-
-
-
-makeHeader(fg)
+ax.legend(["data", "filtered", "down zero crossings", "fit"])
 D.exportfigure(f"FRAME{FRAME_NUMBER}")
+
+
+
+
+
+
+
+
+
+
+
 D.closedocument()
-
-
-
-
-
-
-# ax.plot(T, Y, '-.', color = "r")
-# ax.plot(T, ff(T, *P1), "-", color = fit_color)
-# ax.plot(T[J], Y[J], 'ko', markerfacecolor = 'white', markersize = 8)
-# marker='o', markeredgewidth=1.5, markersize=16
-# ax.legend(["data", "filtered", "fitted", "down zero crossings"])
