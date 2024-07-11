4cb1e04f4145090376f15e83f0367731e7e9a38a
Roch Schanen <r.schanen@lancaster.ac.uk>
2024-07-11 10:39:42 +0100

    fit.py: add file infos

--- fit.py
+++ fit.py
@@ -6,11 +6,16 @@
 
 BLOCK_SIZE = 1<<20 # 1MB
 
+HEADER_SIZE = 4 # file header lines to skip
+
 #############
 ### USAGE ###
 #############
 
 from sys import argv, exit
+
+# all arguments have fixed positions
+# all arguments are required
 
 if len(argv)<2:
     print("""    --- usage ---
@@ -55,11 +60,68 @@ if not validDir(p):
 if n.strip() == "":
     n = splitPath(argv[1])[1]
 
-print(f"{p}/{n}")
+########################
+### OTHER PARAMETERS ###
+########################
 
+f0 = float(argv[3])
+s0 = float(argv[4])
+r0 = float(argv[5])
 
+##################
+### LAST BLOCK ###
+##################
 
+# read last block
+fh = open(fpi, "rb")
+# only use 1KB block: one data line
+# is approximately 30 characters.
+fh.seek(-1024, 2)
+fb = fh.read()
+fh.close()
 
+# convert binary to text
+ft = fb.decode('utf-8')
+
+# convert text to values
+T, V = [], []
+for l in ft.split("\n")[1:]:
+    if l == "": break
+    t, v = l.split(",")
+    T.append(float(t))
+    V.append(float(v))
+
+# GET LAST DATA TIME VALUE
+t_last = T[-1]
+
+###################
+### FIRST BLOCK ###
+###################
+
+# read first block
+fh = open(fpi, "rb")
+# use default block size
+fb = fh.read(BLOCK_SIZE)
+# convert binary to text
+ft = fb.decode('utf-8')
+# convert text to values:
+# skip first four lines
+# the last line is most likely truncated
+T, V, L = [], [], ft.split("\n")[HEADER_SIZE:]
+for l in L[:-1]:
+    t, v = l.split(",")
+    T.append(float(t))
+    V.append(float(v))
+# buffer last line
+ll = L[-1]
+
+# GET FIRST DATA TIME VALUE
+t_first = T[0]
+
+# GET DATA TIME INTERVAL
+t_delta = T[1]-T[0]
+
+print(t_first, t_last, t_delta)
 
 
 
